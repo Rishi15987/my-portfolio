cdbffcd5741e04acbd39b80142707817f08b42a0
Ashutosh Sagar <ashu.sagar111@gmail.com>
2025-03-21 17:34:50 +0530

    intro mobile responsive

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -14,8 +14,8 @@ const Intro = () => {
         <div className="absolute bottom-0 z-[0] left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
         <div className="text-white h-full flex flex-col justify-center items-center gap-6">
             <div>
-                <h1 className="text-6xl py-4 text-center tracking-widest">Rishi Raj</h1>
-                <p className="text-3xl uppercase">Backend Software Engineer</p>
+                <h1 className="text-4xl md:text-6xl py-4 text-center md:tracking-widest">Rishi Raj</h1>
+                <p className="text-2xl md:text-3xl uppercase">Backend Software Engineer</p>
             </div>
             <div className="flex gap-4">
                 <Link href={'https://github.com/Rishi15987'} target="_blank" className="z-10">
