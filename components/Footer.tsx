3c1565c99bf5e0559d49d2e6d65c953e0a57dd6a
Ashutosh Sagar <ashu.sagar111@gmail.com>
2025-03-27 14:43:03 +0530

    skills added animation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import Link from 'next/link';
+import { TbBrandLeetcode } from 'react-icons/tb';
 const Footer = () => {
   return (
     <div className='flex flex-col gap-5 justify-center items-center h-20 bg-zinc-900 text-white px-8 md:px-32'>
         <div className='flex gap-6'>
-            <FaXTwitter size={20}/>
-            <FaLinkedin size={20}/>
-            <FaGithub size={20}/>
+            <Link href={'https://leetcode.com/u/rishirajkrmr789/'}><TbBrandLeetcode size={20}/></Link>
+            <Link href={'https://www.linkedin.com/in/rishi-raj-84a6261a0/'}><FaLinkedin size={20}/></Link>
+            <Link href={'https://github.com/Rishi15987'}><FaGithub size={20}/></Link>
         </div>
         <div className='flex justify-center items-center text-sm border-t border-zinc-500 w-full py-2'>
             <p>© 2025 Rishi Raj</p>
--- components/Skills.tsx
+++ components/Skills.tsx
@@ -17,80 +17,119 @@ import { FaDocker } from "react-icons/fa";
 import { FaPython } from "react-icons/fa";
 const Skills = () => {
   return (
-    <div className="md:h-screen flex items-center bg-zinc-900 text-white">
+    <div className="lg:h-screen flex flex-col items-center justify-center bg-zinc-900 text-white">
     <div className="px-8 md:px-32 flex flex-col gap-10 w-full">
       <motion.div
         initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}  
-        className="mb-12 flex flex-col"
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5}}  
+        className="flex flex-col"
       >
         <h1 className="text-4xl md:text-6xl text-white font-semibold mb-4">Skills</h1>
         <div className="w-24 bg-[#28D08A] h-1"></div>
       </motion.div>
-      <div className="p-4 flex flex-col lg:flex-row gap-6 w-full">
-        <div className="flex flex-col md:flex-row justify-between lg:flex-col lg:w-1/3">
-          <div>
+      <div className="md:p-4 flex flex-col lg:flex-row gap-6 w-full">
+        <div className="flex flex-col justify-between lg:flex-col lg:w-1/3">
+          <motion.div 
+            initial={{ opacity: 0, x: -20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.2}}
             
-              <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">Languages</h1>
-            
-            <div className="flex gap-10 mt-8">
-              <FaJava size={40}/>
-              <TbFileTypeSql size={40}/>
-              <IoLogoJavascript size={40}/>
-              <FaPython size={40}/>
+          >
+            <h1 className=" text-3xl font-semibold tracking-wide text-[#28D08A]">Development</h1>
+            <div className="lg:-translate-x-10 grid grid-cols-3 gap-8 mt-8 py-4">
+              <div className="flex flex-col items-start lg:items-center gap-1">
+                <FaJava size={40}/>
+                <p className="lg:text-center text-left">Java</p>
+              </div>
+              <div className="flex flex-col items-center gap-1">
+                <TbFileTypeSql size={40}/>
+                <p className="text-center">SQL</p>
+              </div>
+              <div className="flex flex-col items-end lg:items-center gap-1">
+                <IoLogoJavascript size={40}/>
+                <p className="text-center">JavaScript</p>
+              </div>
+              <div className="flex flex-col items-start lg:items-center gap-1">
+                <FaPython size={40}/>
+                <p className="text-center">Python</p>
+              </div>
+              <div className="flex flex-col items-center gap-1">
+                <BiLogoSpringBoot size={40}/>
+                <p className="text-center">Spring Boot</p>
+              </div>
+              <div className="flex flex-col items-end lg:items-center gap-1">
+                <SiHibernate size={40}/>
+                <p className="text-center">Hibernate</p>
+              </div>
+              <div className="flex flex-col items-start lg:items-center gap-1">
+                <RiReactjsFill size={40}/>
+                <p className="text-center">React</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">Framework</h1>
-            <div className="flex gap-10 mt-8">
-              <BiLogoSpringBoot size={40}/>
-              <SiHibernate size={40}/>
-              <RiReactjsFill size={40}/>
-            </div>
-          </div>
+          </motion.div>
         </div>
         <div className="flex flex-col lg:w-2/3 gap-10">
-          <div className="flex flex-col md:flex-row justify-between">
+          <motion.div 
+            initial={{ opacity: 0, x: 20 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, delay: 0.2}}
+            className="flex flex-col md:flex-row justify-between"
+          >
             <div >
-              <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">Developer Tools</h1>
-              <div className="flex gap-10 mt-10">
-                <FaGitAlt size={40}/>
-                <SiJira size={40}/>
-                <SiPostman size={40}/>
-                <SiApachemaven size={40}/>
+              <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">DevOps</h1>
+              <div className="flex flex-wrap gap-10 mt-10">
+                <div className="flex flex-col items-center gap-1">
+                  <FaGitAlt size={40}/>
+                  <p className="text-center">React</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <SiJira size={40}/>
+                  <p className="text-center">Jira</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <SiPostman size={40}/>
+                  <p className="text-center">Postman</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <SiApachemaven size={40}/>
+                  <p className="text-center">Maven</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <FaAws size={40}/>
+                  <p className="text-center">AWS</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <FaJenkins size={40}/>
+                  <p className="text-center">Jenkins</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <SiAwslambda size={40}/>
+                  <p className="text-center">Lambda</p>
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <FaDocker size={40}/>
+                  <p className="text-center">Docker</p>
+                </div>
               </div>
             </div>
+          </motion.div>
+          <motion.div 
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.5}}
+            className="flex flex-col md:flex-row justify-between">
             <div>
-              <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">Cloud and CI/CD</h1>
-              <div className="flex gap-10 mt-10">
-                <FaAws size={40}/>
-                <FaJenkins size={40}/>
-                <SiAwslambda size={40}/>
-                <FaDocker size={50}/>
+              <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">Knowledge</h1>
+              <div className="flex flex-col md:flex-row lg:flex-wrap  gap-10 mt-10">
+                <p>RESTful APIs</p>
+                <p>GraphQL</p>
+                <p className="lg:w-24">Microservices Architecture</p>
+                <p className="lg:w-40">Event-Driven Architecture (Kafka)</p>
+                <p className="lg:w-20">Authentication & Security</p>
               </div>
             </div>
-          </div>
-          <div className="flex flex-col md:flex-row justify-between">
-            <div >
-              <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">Developer Tools</h1>
-              <div className="flex gap-10 mt-10">
-                <FaGitAlt size={40}/>
-                <SiJira size={40}/>
-                <SiPostman size={40}/>
-                <SiApachemaven size={40}/>
-              </div>
-            </div>
-            <div>
-              <h1 className="text-3xl font-semibold tracking-wide text-[#28D08A]">Cloud and CI/CD</h1>
-              <div className="flex gap-10 mt-10">
-                <FaAws size={40}/>
-                <FaJenkins size={40}/>
-                <SiAwslambda size={40}/>
-                <FaDocker size={50}/>
-              </div>
-            </div>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
